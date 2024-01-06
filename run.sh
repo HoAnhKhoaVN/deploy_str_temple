906b7aed6d79f6cda7000378442d8e41b2500d71
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-01-06 16:03:13 +0700

    Add code to run

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 source D:/Master/OCR_Nom/deploy/azure/str_vietnam_temple/.venv/Scripts/activate
 
 IMG_PATH='241649023_543306286939449_2854614152112438955_n'
+MODE='img_process'
 
-python3 main.py -i input/$IMG_PATH.jpg \
-                -o output/$IMG_PATH.jpg
+python main.py -i input/$IMG_PATH.jpg \
+                -o output/$IMG_PATH.jpg 
