754b0c2e3ca4364854334822e50aa15f70be6dbd
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-01-07 22:11:35 +0700

    Demo traditional package

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Src: https://youtu.be/Mgp6-ZMEcE0?si=ihkm3YejaIOKiUlg
+from setuptools import setup, find_packages
+
+setup(
+    name = "demo",
+    version= "1.0",
+    packages= find_packages()
+)
