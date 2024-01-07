21a86bd64e39bb1435f5d6bb3ca3cd88b199eaae
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-01-07 14:55:47 +0700

    Add config for blend text

---
+++ my_postprocess/config.py
@@ -0,0 +1,19 @@
+FONT_PATH = 'font/arial.ttf'
+FD_DRAW_TEXT = 'draw_text'
+FD_ROTATE = 'rotate_img'
+PADDING = 2
+LOG = 'log'
+FONT_TEXT = 48
+MAX_HEIGH = 500 #pixel
+MAX_WIDTH = 500 #pixel
+PADDING_IMG_H = 2
+PADDING_IMG_W = 3
+HEIGHT_CHECK_VALID = 80
+WIDTH_CHECK_VALID = 1000
+
+VI_TEXT = 'aăâbcdđeêghiklmnoôơpqrstuưvxyAĂÂBCDĐEÊGHIKLMNOÔƠPQRSTUƯVXY'
+OUT_PATH = 'D:/Master/OCR_Nom/experiments/mapping_font/output/check_valid_font'
+BACKGROUND_IMG_DIR = "input"
+
+VI_LANGUAGE='vi'
+CH_LANGUAGE='ch'
