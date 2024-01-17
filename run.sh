7129e3751cef4fc1949e3fa7cfa03450f700c645
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-01-17 14:36:56 +0700

    Add code predict

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 source D:/Master/OCR_Nom/deploy/azure/str_vietnam_temple/.venv/Scripts/activate
 
 IMG_PATH='241649023_543306286939449_2854614152112438955_n'
-MODE='img_process'
+MODE='rm_text'
+# MODE='fill_color'
 
 python run.py -i input/$IMG_PATH.jpg \
               -o output/"$IMG_PATH"_"$MODE".jpg 
